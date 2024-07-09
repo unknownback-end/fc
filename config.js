dc7f7f4ab08eba07a2b3b51791aa8085bc76e8c6
unknownback-end <142935698+unknownback-end@users.noreply.github.com>
2024-07-08 23:55:59 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,33 @@
+// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
+var config = {
+    // La longitud de las frases puede ser cualquier cosa, puedes escribir diez frases, veinte frases, todas son válidas
+    // Trata de que cada frase no exceda los 15 caracteres para que el efecto de visualización sea mejor
+    texts: [
+        "Para ti,",      // ¡Importante! ¡El último carácter de cada frase debe ser una coma en inglés! ¡Es crucial!
+        "mi querida maria",
+        "Hoy es tu cumpleaños",
+        "y quiero que la pases de lo mejor",
+        "olvida todo lo malo",
+        "hoy es un dia para festejar",
+        "te deseo lo mejor",
+        "con cariño...",
+        "Edward",
+        "TE AMO",
+    ],
+
+    imgs: {
+        "mi querido amorcito": "./imgs/xiaokeai.png",
+        // "Hoy es tu cumpleaños": "./imgs/birthday.jpg",
+    },
+    // Descripción de los botones, a continuación están las descripciones predeterminadas en inglés, puedes cambiarlas a tu gusto
+    desc: {
+        turn_on: "Empezar",
+        play: "Música",
+        bannar_coming: "Color",
+        balloons_flying: "¿Falta algo?",
+        cake_fadein: "¿Pastel?",
+        light_candle: "¿Velas?",
+        wish_message: "Feliz Cumpleaños",
+        story: "UN MENSAJE PARA TI",
+    }
+};
